c0910f24ab5205a2a1a9922501fe9b3316aa6df6
Nmerkel <noramerkel0@gmail.com>
2022-08-29 15:20:47 -0400

    Learning about Array static memory

---
+++ src/main/java/ArrayDemo.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int[] scoreArray=new int[10];
+        System.out.println(Arrays.toString(scoreArray));
+    }
+}
