efe4ef5c70675531439b1d4b4ef268facbeac712
Nmerkel <noramerkel0@gmail.com>
2022-09-22 12:01:22 -0400

    How to use HashMap  to find the first Non repeating char

--- 03.CollectionsReview/src/main/java/MapReview.java
+++ 03.CollectionsReview/src/main/java/MapReview.java
@@ -5,30 +5,33 @@ import java.util.TreeMap;
 public class MapReview {
     public static void main(String[] args) {
     // create hash map
-        Map<Integer,String> studentsMap=new HashMap<>();
-        studentsMap.put(1, "Jack");
-        studentsMap.put(2, "Julia");
-        String st= "JJavvaDDevelloopperr";
+        Map<Integer,String> names=new HashMap<>();
+        names.put(1,"Sara");
+        names.put(2,"Lala");
+        names.put(3,"mike");
+        String str= "life is full of adventures ";
 
-        System.out.println(findFirstNonRepeating(st));
+        System.out.println(findFirstNonRepeating(str));
     }
-    public static Character findFirstNonRepeating(String str){
-      // create a map : this will cost me space complexity O(n)
-        Map<Character,Integer> map= new TreeMap<>();
+    public static Character findFirstNonRepeating(String str) {
+        // create a map : this will cost me space complexity O(n)
+        Map<Character, Integer> map = new HashMap<>();
         int count;
-     // count the frequency of chars
-      for(Character ch:str.toCharArray()){
-          if(map.containsKey(ch)) {
-              count=map.get(ch);
-              map.put(ch, count+1);
-          }
-          else map.put(ch, 1);
-      }
 
-     // start from the beginning and check if there is a char with frequency 1
-        for(Character ch:str.toCharArray()){
+        // count the frequency of chars
+        for (Character ch : str.toCharArray()) {
+            if (map.containsKey(ch)) {
+                count = map.get(ch);
+                map.put(ch, count + 1);
+            } else map.put(ch, 1);
+        }
+        // start from the beginning and check if there is a char with frequency 1
+        for (Character ch : str.toCharArray()) {
             if(map.get(ch)==1) return ch;
         }
         return null;
-    }
-}
+
+
+
+    }}
+
