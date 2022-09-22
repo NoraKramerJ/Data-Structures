7f11be864836130a46b400551d02f44a642953c4
Nmerkel <noramerkel0@gmail.com>
2022-09-22 11:13:21 -0400

    Learning about HashSet with examples

--- 03.CollectionsReview/src/main/java/SetReview.java
+++ 03.CollectionsReview/src/main/java/SetReview.java
@@ -6,14 +6,15 @@ import java.util.concurrent.Callable;
 public class SetReview {
     public static void main(String[] args) {
         //1. create a set
-      Set<Student> set =new LinkedHashSet<>();
+Set<Student> set=new HashSet<>();
         // 2 add element
+        set.add(new Student(1, "Jack"));
+        set.add(new Student(2, "Julia"));
+        set.add(new Student(3, "Mike"));
+        set.add(new Student(4, "Mary"));
 
-        set.add(new Student(1,"Jack"));
-        set.add(new Student(2,"Julia"));
-        set.add(new Student(4,"Mary"));
-        set.add(new Student(3,"Mike"));
-        set.add(new Student(4,"Mary"));
+
+
 
 
         System.out.println(set);
