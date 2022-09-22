f46a44905a269ccd89d0caba62eb86314c325052
Nmerkel <noramerkel0@gmail.com>
2022-09-22 11:39:25 -0400

    How to use HashSet to find the repeating char

--- 03.CollectionsReview/src/main/java/SetReview.java
+++ 03.CollectionsReview/src/main/java/SetReview.java
@@ -1,7 +1,6 @@
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
-import java.util.concurrent.Callable;
 
 public class SetReview {
     public static void main(String[] args) {
@@ -27,24 +26,25 @@ Set<Student> set=new HashSet<>();
         System.out.println(set2.add(new Student(4,"Mary")));
         //the print statement above will return False as no duplicates will be added for LinkedHashSet
         System.out.println(set2);
-        //String st= "Javva Developer";
+        //String st= "Java Developer";
 
         //System.out.println(firstRepeatingChar(st));
 
     }
-    public static Character firstRepeatingChar(String str){
+    public static Character firstRepeatingChar(String str) {
         // What is the time complexity of the following code? O(n)
 
 
         // create a hashSet
-        Set<Character> chars=new HashSet<>(); // in here I have Space Complexity of O(n)
+        Set<Character> chars = new HashSet<>(); // in here I have Space Complexity of O(n)
         // iterate over the char array and add chars into hashSet
 
+        for (Character ch : str.toCharArray()) if(!chars.add(ch)) return ch;
 
-        for(Character ch:str.toCharArray()) if (!chars.add(ch)) return ch;
-        // if add ops is false return that char
 
        return null;
+        // if add ops is false return that char
+
     }
 
 }
